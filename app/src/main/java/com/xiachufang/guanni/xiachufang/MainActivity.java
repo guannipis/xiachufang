eb14f8d5d4e851f8717a0647d29d986f201d0725
duanfazhu <bird10080@163.com>
2016-08-25 20:21:47 +0800

    Initial commit

---
+++ app/src/main/java/com/xiachufang/guanni/xiachufang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xiachufang.guanni.xiachufang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
